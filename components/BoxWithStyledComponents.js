373dabd19fa414853b1e6dd2629a6df1a1665283
Marcus Bohmann <mbohmann@gmx.de>
2023-06-06 12:37:42 +0200

    add styled components

---
+++ components/BoxWithStyledComponents.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: ${({ isBlack }) => (isBlack ? "black" : "green")};
+  margin: 2rem;
+
+  &:hover {
+    background-color: red;
+  }
+`;
+
+export default StyledBox;
+
+/* export default function BoxWithStyledComponents({ isBlack }) {
+  return <StyledBox black={isBlack}></StyledBox>;
+}
+ */
